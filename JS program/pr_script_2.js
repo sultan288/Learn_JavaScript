6906fefa4b7f4c0ffdc66e3770cda56397e84617
sultan288 <sultan.ist28@gmail.com>
2021-06-24 11:13:52 +0600

    jsupdate24jun

---
+++ JS program/pr_script_2.js
@@ -0,0 +1,22 @@
+// Task 2:
+// get first name from User 
+// get last name from user 
+// add without using library function 
+// print full name 
+// total length of full name 
+// convert full name into uppercase
+// print the second position from full name 
+
+
+// var firstName = prompt("Enter your first name: ");
+// var lastName = prompt("Enter your last name: ");
+// var fullName = firstName+" "+lastName;
+// document.write(fullName);
+// document.write(fullName.length);
+
+// var fullName = "Tipu Sultan";
+// var uc = fullName.toUpperCase(fullName);
+// document.write(uc);
+
+// document.write(fullName.charAt(1));
+
